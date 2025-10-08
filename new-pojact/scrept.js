1acdb180cde15a6c6c5d4c5fb67601ab73026f59
bdmobileshop701-a11y <bdmobileshop701@gmail.com>
2025-10-08 10:49:51 +0600

    Add files via upload

---
+++ new-pojact/scrept.js
@@ -0,0 +1,13 @@
+let inp = document.getElementById("input")
+let button = document.getElementById("butn")
+
+
+form.addEventListener("submit",printData)
+
+function printData() {
+event.preventDefault()
+      let data =input.value;
+
+
+      console.log(data)
+}
